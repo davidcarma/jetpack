1023c43e9a2bbc2f5870533324f1e689a7aa9432
Mikael Korpela <mikael.korpela@automattic.com>
2026-02-05 12:50:02 +0200

    Validate dates before processing

--- projects/packages/forms/routes/responses/stage.tsx
+++ projects/packages/forms/routes/responses/stage.tsx
@@ -344,6 +344,11 @@ function StageInner() {
 						endDate = new Date( dateValue );
 					}
 
+				// Validate dates before processing
+				if (
+					! isNaN( startDate.getTime() ) &&
+					! isNaN( endDate.getTime() )
+				) {
 					startDate.setUTCHours( 0, 0, 0, 0 );
 					endDate.setUTCHours( 23, 59, 59, 999 );
 
@@ -367,6 +372,7 @@ function StageInner() {
 							queryArgs.before = endOfDayISO;
 							break;
 					}
+				}
 				}
 			}
 		} );
